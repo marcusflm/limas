1fd52c9f02e4bef51e628de4d764a7553d066d58
Marcus Mendes <marcusflm@gmail.com>
2023-08-23 03:46:39 +0000

    wip

--- resources/views/livewire/cliente/create.blade.php
+++ resources/views/livewire/cliente/create.blade.php
@@ -3,10 +3,10 @@
     <div class="w-1/2 mx-auto">
         <x-card shadow>
             <x-form wire:submit="save">
-                <x-input label="Nome" wire:model="cliente.nome" required />
-                <x-input label="Telefone" wire:model="cliente.telefone" placeholder="(99) 9 9999-9999" x-mask="(99) 9 9999-9999" maxlength="11" required />
-                <x-input label="E-mail" wire:model="cliente.email" placeholder="email@example.com" required />
-                <x-select label="Bairro" wire:model="cliente.bairro_id" placeholder="Selecione um bairro" :options="$bairros" option-value="id" option-label="nome" />
+                <x-input label="Nome" wire:model="nome" required />
+                <x-input label="Telefone" wire:model="telefone" placeholder="(99) 9 9999-9999" x-mask="(99) 9 9999-9999" required />
+                <x-input label="E-mail" wire:model="email" placeholder="email@example.com" required />
+                <x-select label="Bairro" wire:model="bairro_id" placeholder="Selecione um bairro" :options="$bairros" option-value="id" option-label="nome" />
                 <x-slot:actions>
                     <x-button label="Cancelar" @click="$wire.navegar('/clientes')" />
                     <x-button label="Salvar" class="btn-primary" type="submit" spinner="save" />
--- resources/views/livewire/cliente/edit.blade.php
+++ resources/views/livewire/cliente/edit.blade.php
@@ -4,11 +4,11 @@
         <x-card shadow>
             <x-form wire:submit="save">
                 <x-input label="Nome" wire:model="nome" required />
-                <x-input label="Telefone" placeholder="(99) 9 9999-9999" x-mask="(99) 9 9999-9999" wire:model="telefone" required />
-                <x-input label="E-mail" placeholder="email@email.com" wire:model="email" email required />
-                <x-select label="Bairro" placeholder="Selecione um bairro" :options="$bairros" option-value="id" option-label="nome" wire:model="bairro_id" />
+                <x-input label="Telefone" wire:model="telefone" placeholder="(99) 9 9999-9999" x-mask="(99) 9 9999-9999" required />
+                <x-input label="E-mail" wire:model="email" placeholder="email@example.com" required />
+                <x-select label="Bairro" wire:model="bairro_id" placeholder="Selecione um bairro" :options="$bairros" option-value="id" option-label="nome" />
                 <x-slot:actions>
-                    <x-button label="Cancelar" wire:click="navegar('/clientes')" />
+                    <x-button label="Cancelar" @click="$wire.navegar('/clientes')" />
                     <x-button label="Salvar" class="btn-primary" type="submit" spinner="save" />
                 </x-slot:actions>
             </x-form>
