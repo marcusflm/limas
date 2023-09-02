280042e8243cf4430afe0de67779ae0e593a512f
Marcus Mendes <marcusflm@gmail.com>
2023-09-02 03:14:30 +0000

    corrige modal pedido create

--- resources/views/livewire/pedido/create.blade.php
+++ resources/views/livewire/pedido/create.blade.php
@@ -4,7 +4,7 @@
         <label for="data_pedido" class="pb-0 label label-text font-semibold">Data pedido</label>
         <input type="date" wire:model="data_pedido" class="input input-primary w-full peer" />
         <x-slot:actions>
-            <x-button label="Cancelar" wire:click="$parent.myModal = false" />
+            <x-button label="Cancelar" wire:click="$dispatch('pedido-edicao-concluida')" />
             <x-button label="Cadastrar" class="btn-primary" type="submit" spinner="save" />
         </x-slot:actions>
     </x-form>
