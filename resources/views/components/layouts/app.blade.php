4394a9e7abfb1da511621369f708ab2f36cd7b41
Marcus Mendes <marcusflm@gmail.com>
2023-09-26 00:11:04 +0000

    ajusta side bar para mobile

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,7 +8,6 @@
     </head>
 
     <body class="min-h-screen bg-base-200 font-sans antialiased">
-        @auth
             <x-nav sticky full-width class="md:hidden lg:hidden">
                 <x-slot:brand>
                     <!-- Drawer toggle for "main-drawer" -->
@@ -18,6 +17,7 @@
                 </x-slot>
             </x-nav>
             <x-main>
+        @auth
                 <x-slot:sidebar class="strong bg-[#c58a25] text-white" drawer="main-drawer" collapsible collapse-text="Recolher">
                     <!-- Hidden when collapsed -->
                     <a href="/">
@@ -45,12 +45,12 @@
                         <x-menu-item title="Usuários" icon="o-face-smile" link="/usuarios" />
                     </x-menu>
                 </x-slot>
-
+        @endauth
                 <x-slot:content>
                     {{ $slot }}
                 </x-slot>
+
             </x-main>
-        @endauth
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <x-livewire-alert::scripts />
